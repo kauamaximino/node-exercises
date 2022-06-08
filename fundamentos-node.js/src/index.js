4d7bf78652cdbdef1032dced448b2fd67ab320ca
Kauã Maximino <kaua_maximino@outlook.com>
2022-06-08 19:06:55 -0300

    desenvolvendo funcionalidade para criação de conta

--- fundamentos-node.js/src/index.js
+++ fundamentos-node.js/src/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const { v4: uuidv4 } = require("uuid")
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "teste World" });
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  
+  const customerAlreadyExists = customers.some(
+    customer => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({
+      error: "Customer already exists"
+    });
+  }
+  
+  const id = uuidv4();
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: []
+  });
+
+  return response.status(201).send()
 });
 
 app.listen(3333);
