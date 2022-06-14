9458c256b609e9fa2c51cc79268b5c43d3a0b815
Kauã Maximino <kaua_maximino@outlook.com>
2022-06-14 15:12:36 -0300

    :sparkles: feat: instalando typescript

--- Rocketseat/Typescript/src/server.ts
+++ Rocketseat/Typescript/src/server.ts
@@ -2,4 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.listen
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World" });
+})
+
+app.listen(3333);
